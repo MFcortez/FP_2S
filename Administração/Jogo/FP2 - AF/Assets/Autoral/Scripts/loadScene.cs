67c13c348f7d547b0d190be2703704a18a174e05
GabrielGarciaRibeiroDeSouza <gabriel.garcia.ribeiro@gmail.com>
2020-10-07 22:07:51 -0300

    -rascunhos do LVL01 e ui main menu

---
+++ Administração/Jogo/FP2 - AF/Assets/Autoral/Scripts/loadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class loadScene : MonoBehaviour
+{
+    
+  
+
+   public void LoadScene (string load)
+    {
+        SceneManager.LoadScene(load);
+        Time.timeScale = 1;
+        
+    }
+}
---
+++ Administração/Jogo/FP2 - AF/Assets/Autoral/Scripts/quitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitGame : MonoBehaviour
+{
+    // só sai do jogo quando está Buildado.
+    public void Exit()
+    {
+        Debug.Log("*Saiu*");
+        Application.Quit();
+    }
+}
